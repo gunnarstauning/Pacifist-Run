3979ad905e5afcb90a8068ca87e7f8648e05b745
ChristopherBuilderman <61890951+ChristopherBuilderman@users.noreply.github.com>
2021-09-14 23:50:37 -0600

    Added Patrol, and much more
    Enemies now Patrol along a route included in their prefab, and will chase the player if the player gets in range. If the player leaves their range they will return to their patrol.

---
+++ Pacifist Mode/Assets/Scripts/EnemyScripts/PatrolState.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolState: State
+{
+    public PatrolState(ChasePlayer chasePlayer) : base(chasePlayer) { }
+
+    private Vector3 destinationPoint;
+
+    public override void CheckTransitions() 
+    {
+        if (chasePlayer.CheckIfInRange("Player"))
+        {
+            chasePlayer.SetState(new ChaseState(chasePlayer));
+        }
+    }
+
+    public override void Act()
+    {
+        if (chasePlayer.agent == null || chasePlayer.agent.remainingDistance > 0.5)
+        {
+            destinationPoint = new Vector3 (chasePlayer.GetNavPoint().position.x, chasePlayer.GetNavPoint().position.y, chasePlayer.GetNavPoint().position.z);
+            chasePlayer.agent.SetDestination(destinationPoint);
+        } else if (chasePlayer.agent.remainingDistance < 0.5)
+        {
+            chasePlayer.GetNextNavPoint();
+            destinationPoint = new Vector3 (chasePlayer.GetNavPoint().position.x, chasePlayer.GetNavPoint().position.y, chasePlayer.GetNavPoint().position.z);
+            chasePlayer.agent.SetDestination(destinationPoint);
+        }
+    }
+
+    public override void OnStateEnter()
+    {
+        destinationPoint = new Vector3 (chasePlayer.GetNavPoint().position.x, chasePlayer.GetNavPoint().position.y, chasePlayer.GetNavPoint().position.z);
+        chasePlayer.agent.SetDestination(destinationPoint);
+        if (chasePlayer.agent != null)
+        {
+            chasePlayer.agent.speed = 5f;
+        }
+        chasePlayer.ChangeColor(Color.blue);
+    }
+}
