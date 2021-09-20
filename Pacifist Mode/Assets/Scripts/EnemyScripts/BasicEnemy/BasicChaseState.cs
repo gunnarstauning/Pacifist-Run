e2cc4164da064f261b83a1d100795bd2a2e0b507
maxPoole15 <64996563+maxPoole15@users.noreply.github.com>
2021-09-20 16:20:09 -0700

    New Hit Animation

--- Pacifist Mode/Assets/Scripts/EnemyScripts/BasicEnemy/BasicChaseState.cs
+++ Pacifist Mode/Assets/Scripts/EnemyScripts/BasicEnemy/BasicChaseState.cs
@@ -23,11 +23,38 @@ public class BasicChaseState : State
     }
     public override void Act() 
     {
+	
+	
+	if (stateMachine.CheckIfInRange("Player", 4f))
+		{
+			stateMachine.anim.SetBool("isAttacking", true);
+			stateMachine.agent.speed = 0f;
+		}
+		else if (stateMachine.CheckIfInRange("BasicEnemy", 4f))
+		{
+			stateMachine.anim.SetBool("isAttacking", true);
+			stateMachine.agent.speed = 0f;
+		}
+		else if (stateMachine.CheckIfInRange("ShootEnemy", 4f))
+		{
+			stateMachine.anim.SetBool("isAttacking", true);
+			stateMachine.agent.speed = 0f;
+		}
+		else
+		{
+		stateMachine.anim.SetBool("isAttacking", false);
+		stateMachine.agent.speed = 7f;
+		}
+		
+		
+	
         if (stateMachine.enemyToChase != null)
         {
             destinationPoint = new Vector3 (stateMachine.enemyToChase.transform.position.x, stateMachine.enemyToChase.transform.position.y, stateMachine.enemyToChase.transform.position.z);
             stateMachine.agent.SetDestination(destinationPoint);
         }
+		
+		
     }
 
     public override void OnStateEnter()
