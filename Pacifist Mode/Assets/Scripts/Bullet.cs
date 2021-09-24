e4740dfc1a503ce20cbe291d8b3b8c6e2353208e
Gunnar Stauning <gunnarstauning@u.boisestate.edu>
2021-09-24 14:46:26 -0600

    BIG bullet push (READY FOR DESIGN)

--- Pacifist Mode/Assets/Scripts/Bullet.cs
+++ Pacifist Mode/Assets/Scripts/Bullet.cs
@@ -4,17 +4,61 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    private int bulletTestTimer = 0;
+    private bool hittable = false;
+
+    //For if bullet doesn't hit anything
+    private int bulletDeathTimer = 0;
+    private int bulletFinalDeath = 3000;
+
+    private void Update()
+    {
+        if (bulletDeathTimer > bulletFinalDeath)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            bulletDeathTimer++;
+        }
+        if (bulletTestTimer > 30)
+        {
+            hittable = true;
+        }
+        else
+        {
+            bulletTestTimer++;
+        }
+    }
     private void OnBecomeInvisible()
     {
         Destroy(gameObject);
     }
 
-    private void OnTriggerEnter (Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        if(other != this)
+        if (hittable)
         {
-            Destroy(gameObject);
+            if (other != this)
+            {
+                Destroy(gameObject);
+                if(other.gameObject.tag != "Environment")
+                {
+                    if (other.gameObject.tag == "Player")
+                    {
+                        Debug.Log("Player Took a Hit!");
+                    }
+                    else
+                    {
+                        Destroy(other.gameObject);
+                    }
+                }
+            }
         }
-        
     }
 }
+
+
+
+
+
